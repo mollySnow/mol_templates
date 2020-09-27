50613e29a96d50aa9877eb5dd27aaa784a0af336
Molly <molly.k.bjork@gmail.com>
2020-09-27 18:40:02 +0200

    generated rust template

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,44 @@
-mod rust;
 mod base;
+mod rust;
 
-pub mod tools
-{
+pub mod tools {
     use crate::rust::*;
 
-    pub fn compile_library_template() -> Result<(), Box<dyn std::error::Error>> 
-    { 
+    pub fn compile_library_template() -> Result<(), Box<dyn std::error::Error>> {
         compile_file("./src/base.rs", "./src/templates/library_template.rs")
     }
-    pub fn update_rust_library() -> Result<(), Box<dyn std::error::Error>> 
-    { 
+    pub fn update_rust_library() -> Result<(), Box<dyn std::error::Error>> {
         let s = super::templates::library_template::render("/*-", "-*/");
         std::fs::copy("./src/rust.rs", "./src/rust.rs.bak")?;
         std::fs::write("./src/rust.rs", s)?;
         Ok(())
-    } 
+    }
 
-    pub fn create_language_library(language_id: &str, marker_begin: &str, marker_end: &str) -> Result<(), Box<dyn std::error::Error>>
-    {
+    pub fn create_language_library(
+        language_id: &str,
+        marker_begin: &str,
+        marker_end: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let render = super::templates::library_template::render(marker_begin, marker_end);
         let p = format!("./src/templates/template_{}.rs", language_id);
+
         let _ = std::fs::copy(&p, format!("{}.bak", &p));
         std::fs::write(p, render)?;
-        find_and_insert_many("./src/lib.rs", "mod_templates", &format!("pub mod template_{};", language_id))?;
+
+        let f = std::fs::read_to_string("./src/lib.rs")?;
+        let lang_module = format!("pub mod template_{};", language_id);
+
+        if !f.contains(&lang_module) {
+            find_and_insert_many("./src/lib.rs", "mod_templates", &lang_module)?;
+        }
+
         Ok(())
     }
 }
 
-pub mod templates
-{
+pub mod templates {
     /*-insert mod_templates-*/
-    pub mod template_html;
     pub mod library_template;
+    pub mod template_html;
+    pub mod template_rust;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    mol_templates::tools::create_language_library("html", "<!--|", "|-->")
+    mol_templates::tools::create_language_library("rust", "/*-", "-*/")
     // mol_templates::tools::update_rust_library()
     // mol_templates::tools::compile_library_template()
 }
